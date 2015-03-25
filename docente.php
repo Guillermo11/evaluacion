6c570185812f56f4cbc33e92b1992d1d6d7ae95e
Motedyn <motedyn@gmail.com>
2015-03-24 20:07:42 -0600

    modulos
    creacion de modulos, fase 1

---
+++ docente.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- <link href='http://fonts.googleapis.com/css?family=Raleway:400,200,600' rel='stylesheet' type='text/css'> -->
+	<link rel="stylesheet" type="text/css" href="cssED/index.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="fonts/helvetica">
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+
+	<meta charset="UTF-8">
+	<title>Evaluacion Docente UMG</title>
+</head>
+
+<body>
+	
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- <link href='http://fonts.googleapis.com/css?family=Raleway:400,200,600' rel='stylesheet' type='text/css'> -->
+	<link rel="stylesheet" type="text/css" href="cssED/index.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="fonts/helvetica">
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+
+	<meta charset="UTF-8">
+	<title>Evaluacion Docente UMG</title>
+</head>
+<body id="body1">
+	<div id="logo">
+		<img src="icon/logoUMG.png" alt="" width="150" height="auto">
+	</div>
+	<div id="login">
+		<h3>Bienvenido</h3>
+		<form id="login2" action="" method="POST" role="form">
+		<!-- <label for="">Usuario</label> -->
+		<br>
+		<input class="form-control" type="text" autofocus="" required="" placeholder="Nombre de usuario">
+		<br>
+		<!-- <label for="">Contraseña</label> -->
+		<input class="form-control" type="password" autofocus="" required="" placeholder="Contraseña">
+		<br>
+		<button type="submit" class="btn">Iniciar sesión</button>
+		</form>
+	</div>
+</body>
+</html>
