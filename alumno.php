8b75091d8548b621415c38876bf988c970ad42d6
Motedyn <motedyn@gmail.com>
2015-03-29 17:24:48 -0600

    actualizacion
    actualizacion

--- alumno.php
+++ alumno.php
@@ -2,17 +2,72 @@
 <html lang="en">
 <head>
 	<!-- <link href='http://fonts.googleapis.com/css?family=Raleway:400,200,600' rel='stylesheet' type='text/css'> -->
-	<link rel="stylesheet" type="text/css" href="cssED/index.css">
+	<link rel="stylesheet" type="text/css" href="cssED/alumno.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="fonts/helvetica">
+	<script type="text/javascript" src="js/jquery.js"></script>
 	<script type="text/javascript" src="js/bootstrap.js"></script>
-
 	<meta charset="UTF-8">
 	<title>Evaluacion Docente UMG</title>
 </head>
 
-<body>
-	
+<body class="bodyalumno">
+<!--barra menu-->
+	<div id="menutop">
+		<nav class="navbar navbar-default">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			<img src="icon/logoUMG.png" alt="" width="100" height="auto">
+			</div>
+		
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
+				<ul class="nav navbar-nav">
+					<h3>Bienvenido:</h3>
+					<h5>uusario xxxxx 2690-234-234234</h5>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<a href="index.php">Cerrar Sesion</a>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</nav>
+	</div>
+<!--barra menu-->
+
+
+	<div id="tabla">
+		<table class="table table-bordered table-hover table-striped">
+			<thead>
+				<tr>
+					<th>Codigo</th>
+					<th>Curso</th>
+					<th>Catedrático</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>2690-238</td>
+					<td>Estadistica Inferencial</td>
+					<td>Ing. Juan Carlos Ordoñez</td>
+				</tr>
+				<tr>
+					<td>2690-345</td>
+					<td>Analisis de Sistemas</td>
+					<td>Ing. Durwin Nose</td>
+				</tr>
+				<tr>
+					<td>2690-235</td>
+					<td>Diseño de Sistemas</td>
+					<td>Ing. Durwin Nose</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </body>
 
 </html>
